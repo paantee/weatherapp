c91fa86109f7d7dc621294e23c16440ae413a4e2
Paantee <teemu-paananen@hotmail.com>
2022-12-16 00:52:20 +0200

    fixed wrong value crashing site

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import WeatherInput from './components/WeatherInput';
+
 function App() {
   return (
     <div>
--- src/components/WeatherInput.js
+++ src/components/WeatherInput.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './WeatherInput.css';
+import './WeatherInput.css'
 
 export default function WeatherInput() {
 
@@ -8,13 +8,19 @@ export default function WeatherInput() {
     const [img, setImg] = useState('');
 
     const imgSrc = 'https://openweathermap.org/img/wn/' + img + '@2x.png';
+
+
+
     const handleChange = event => {
         setLocation(event.target.value);
     };
 
     const handleSubmit = event => {
+        if (weather == undefined) {
+            window.alert("Name a country or a city")
+        } else
+            fetchData();
         event.preventDefault();
-        fetchData();
     }
 
     const fetchData = () => {
@@ -32,22 +38,25 @@ export default function WeatherInput() {
     useEffect(() => fetchData(), []);
 
     return (
-        <div class="div1">
+
+        <div className="div1">
             <span>
                 <h1>Enter Location</h1>
                 <form onSubmit={handleSubmit}>
                     <input type='text' id='location' value={location} onChange={handleChange} />
                     <button type='submit'>Submit</button>
                 </form>
+
                 {weather && (
                     <div>
-                        <h2>Current weather in {weather.name}:</h2>
-                        <p>Temperature: {weather.main.temp}°C</p>
-                        <p>Conditions: {weather.weather[0].description}</p>
-                        <img src={imgSrc} alt={weather.weather[0].description} />
+                        <h2>Current weather in {weather?.name}:</h2>
+                        {weather.main ? <p>Temperature: {weather?.main?.temp}°C</p> : null}
+                        {weather.weather ? <p>Conditions: {weather.weather[0].description}</p> : null}
+                        {weather.weather ? <img src={imgSrc} alt={weather?.weather[0].description} /> : null}
                     </div>
                 )}
             </span>
         </div>
+
     );
 }
